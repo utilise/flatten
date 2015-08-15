1e58c4a6ee7a5b6b777ef0ce66ca2e9c391c40f0
pemrouz <p3pedram@hotmail.com>
2015-08-15 15:37:47 +0100

    flatten recursively

--- test.js
+++ test.js
@@ -5,4 +5,21 @@ describe('flatten', function() {
   it('should flatten array', function() {
     expect([[1],[2],[3]].reduce(flatten)).to.be.eql([1,2,3])
   })
+
+  it('should flatten array recursively', function() {
+    expect([[[1,1]],[2],[3]].reduce(flatten, [])).to.be.eql([1,1,2,3])
+  })
+
+  it('should flatten array - 0', function() {
+    expect([].reduce(flatten, [])).to.be.eql([])
+  })
+
+  it('should flatten array - 1', function() {
+    expect([[1]].reduce(flatten, [])).to.be.eql([1])
+  })
+
+  it('should flatten array - 2', function() {
+    expect([[1],[2]].reduce(flatten, [])).to.be.eql([1,2])
+  })
+
 })
